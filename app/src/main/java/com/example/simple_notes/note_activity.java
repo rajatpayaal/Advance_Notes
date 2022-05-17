b50f368c776ed6962ff3123a0fbd6a64759075df
rajatpayaal <rajatpayal@outlook.in>
2022-05-18 01:04:37 +0530

    notexml

---
+++ app/src/main/java/com/example/simple_notes/note_activity.java
@@ -0,0 +1,14 @@
+package com.example.simple_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class note_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note);
+    }
+}
