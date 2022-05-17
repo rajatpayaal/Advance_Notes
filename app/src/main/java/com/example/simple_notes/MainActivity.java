927461962e0b9d304ac3bed007d58df86d4d35dc
rajatpayaal <rajatpayal@outlook.in>
2022-05-17 19:23:43 +0530

    start assign

---
+++ app/src/main/java/com/example/simple_notes/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.simple_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+//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
