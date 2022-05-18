8cf2bb05ad46a586fdd7b0e0f3bf8e3206b4d461
rajatpayaal <rajatpayal@outlook.in>
2022-05-18 22:01:58 +0530

    buttonwork create new note

--- app/src/main/java/com/example/simple_notes/MainActivity.java
+++ app/src/main/java/com/example/simple_notes/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.simple_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUES_CODE_ADD_NOTE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView newnote =findViewById(R.id.newnote);
+                newnote.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivityForResult(
+                             new Intent(getApplicationContext(), note_activity.class),
+                                REQUES_CODE_ADD_NOTE
+                        );
+                    }
+                });
     }
 }
 
--- app/src/main/java/com/example/simple_notes/note_activity.java
+++ app/src/main/java/com/example/simple_notes/note_activity.java
@@ -3,6 +3,8 @@ package com.example.simple_notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class note_activity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class note_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        ImageView imageback =findViewById(R.id.imageback);
+        imageback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+
     }
 }
