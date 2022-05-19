58a3a3a1dac6a08a7150e735286f5dd65ec1c211
rajatpayaal <rajatpayal@outlook.in>
2022-05-20 02:05:23 +0530

    noteperfectsavedb

--- app/src/main/java/com/example/simple_notes/acti_pack/note_activity.java
+++ app/src/main/java/com/example/simple_notes/acti_pack/note_activity.java
@@ -3,6 +3,8 @@ package com.example.simple_notes.acti_pack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,13 +13,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.simple_notes.R;
+import com.example.simple_notes.db.Database.NoteDatabase;
+import com.example.simple_notes.entity.Note;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 public class note_activity extends AppCompatActivity {
-private EditText inputeNotetitle ,InputeNote,InputNoteSubtitle, InputeNoteText;
+private EditText inputeNotetitle ,InputeNote,InputNoteSubtitle,InputeNoteText;
 private TextView textDateTime;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,13 +50,49 @@ private TextView textDateTime;
                 new SimpleDateFormat("EEEE,dd MMMM yyy HH:mm a",
                         Locale.getDefault()).format(new Date())
         );
+
+        ImageView imagedone=findViewById(R.id.imagedone);
+                imagedone.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                     saveNote();
+                    }
+                });
     }
 
-//private void saveNote(){
-//        if(inputeNotetitle.getText().toString().trim().isEmpty()){
-//            Toast.makeText(this,"Note title not empty",Toast.LENGTH_SHORT).show();
-//            return;
-//
-//        }else if (inputeNotetitle.getText().toString())
-//}
+private void saveNote(){
+        if(inputeNotetitle.getText().toString().trim().isEmpty()){
+            Toast.makeText(this,"Note title not empty",Toast.LENGTH_SHORT).show();
+            return;
+
+        }else if (inputeNotetitle.getText().toString().trim().isEmpty()
+        && InputeNote.getText().toString().trim().isEmpty()){
+            Toast.makeText(this,"Note can't be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note=new Note();
+        note.setTitle(inputeNotetitle.getText().toString());
+        note.setSubtitle(InputNoteSubtitle.getText().toString());
+        note.setNoteText(InputeNoteText.getText().toString());
+        note.setDatetime(textDateTime.getText().toString());
+
+        class SaveNoteText extends AsyncTask<Void,Void,Void> {
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NoteDatabase.getNoteDatabase(getApplicationContext()).notedao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent=new Intent();
+                setResult(RESULT_OK,intent);
+                finish();
+            }
+        }
+
+new SaveNoteText().execute();
+}
     }
