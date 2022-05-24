0c757f5addf638fbda66d0f4f9a4d4ba6721465e
rajatpayaal <rajatpayal@outlook.in>
2022-05-25 02:29:57 +0530

    note_built_gradel

--- app/src/main/java/com/example/simple_notes/adapters/NoteAdapter.java
+++ app/src/main/java/com/example/simple_notes/adapters/NoteAdapter.java
@@ -4,6 +4,8 @@ import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,31 +13,60 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.simple_notes.R;
 import com.example.simple_notes.entity.Note;
+import com.example.simple_notes.listeners.NotesListeners;
 
 import java.util.List;
 
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.makeramen.roundedimageview.RoundedImageView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder> {
 
-    private List<Note>notes;
+    private List<Note> notes;
+    private final List<Note> notesSource;
+    private final NotesListeners notesListener;
 
-    public NoteAdapter(List<Note> notes) {
+    private Timer timer;
+
+    public NoteAdapter(List<Note> notes, NotesListeners notesListener) {
         this.notes = notes;
+        this.notesListener = notesListener;
+        notesSource = notes;
     }
 
     @NonNull
     @Override
     public NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new NoteViewHolder(
-                LayoutInflater.from(parent.getContext()).inflate(
-                        R.layout.item_container_note,
-                        parent,false
-                )
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_container_note, parent, false)
         );
     }
 
     @Override
     public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
-      holder.setNote(notes.get(position));
+        holder.setNote(notes.get(position));
+        holder.layoutNote.setOnClickListener(v ->
+                notesListener.onNoteClicked(notes.get(position), position));
     }
 
     @Override
@@ -48,27 +79,73 @@ public class NoteAdapter extends RecyclerView.Adapter<NoteAdapter.NoteViewHolder
         return position;
     }
 
-    static class NoteViewHolder extends RecyclerView.ViewHolder{
-        TextView textTitle,textSubtitle, textDateTime;
+    static class NoteViewHolder extends RecyclerView.ViewHolder {
 
-        NoteViewHolder(@NonNull View itemView){
+        TextView textTitle, textSubtitle, textDateTime;
+        LinearLayout layoutNote;
+        RoundedImageView imageNote;
+
+        NoteViewHolder(@NonNull View itemView) {
             super(itemView);
-            textTitle=itemView.findViewById(R.id.InputNoteTitle);
-            textSubtitle=itemView.findViewById(R.id.InputeNoteSublite);
-            textDateTime=itemView.findViewById(R.id.textDateTime);
+            textTitle = itemView.findViewById(R.id.textTitle);
+            textSubtitle = itemView.findViewById(R.id.textSubtitle);
+            textDateTime = itemView.findViewById(R.id.textDateTime);
+            layoutNote = itemView.findViewById(R.id.layoutNote);
+            imageNote = itemView.findViewById(R.id.imageNote);
         }
-        void setNote(Note note){
-            textTitle.setText(note.getNoteText());
-            if(note.getSubtitle().trim().isEmpty()) {
+
+        void setNote(Note note) {
+            textTitle.setText(note.getTitle());
+            if (note.getSubtitle().trim().isEmpty()) {
                 textSubtitle.setVisibility(View.GONE);
-            }
-            else {
+            } else {
                 textSubtitle.setText(note.getSubtitle());
             }
             textDateTime.setText(note.getDatetime());
 
+            GradientDrawable gradientDrawable = (GradientDrawable) layoutNote.getBackground();
+            if (note.getColor() != null) {
+                gradientDrawable.setColor(Color.parseColor(note.getColor()));
+            } else {
+                gradientDrawable.setColor(Color.parseColor("#333333"));
+            }
 
+            if (note.getImage_path() != null) {
+                imageNote.setImageBitmap(BitmapFactory.decodeFile(note.getImage_path()));
+                imageNote.setVisibility(View.VISIBLE);
+            } else {
+                imageNote.setVisibility(View.GONE);
             }
         }
-
     }
+
+    public void searchNotes(final String searchKeyword) {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if (searchKeyword.trim().isEmpty()) {
+                    notes = notesSource;
+                } else {
+                    ArrayList<Note> temp = new ArrayList<>();
+                    for (Note note : notesSource) {
+                        if (note.getTitle().toLowerCase().contains(searchKeyword.toLowerCase()) ||
+                                note.getSubtitle().toLowerCase().contains(searchKeyword.toLowerCase()) ||
+                                note.getNoteText().toLowerCase().contains(searchKeyword.toLowerCase())) {
+                            temp.add(note);
+                        }
+                    }
+                    notes = temp;
+                }
+
+                new Handler(Looper.getMainLooper()).post(() -> notifyDataSetChanged());
+            }
+        }, 500);
+    }
+
+    public void cancelTimer() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
+}
